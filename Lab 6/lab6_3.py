535df3428a9bbc7ab683fa15baf363585d7922b9
mariazhak <mariia.zhak@auk.edu.ua>
2024-03-14 16:34:02 +0200

    Changed task 3

--- Lab 6/lab6_3.py
+++ Lab 6/lab6_3.py
@@ -1,16 +1,19 @@
+def wc(f):
+    data = f.readlines()
+    print('The amount of lines is', len(data))
+
+    # counting the amount of words
+    words = []
+    for line in data:
+        words.extend(line.split(' '))
+    print('The amount of words is', len(words))
+
+    # counting the amount of characters
+    characters = 0
+    for line in data:
+        characters += len(line)
+    print('The amount of characters is', characters)
+
 f = open('learning_python.txt')
-data = f.readlines()
-print('The amount of lines is', len(data))
-
-# counting the amount of words
-words = []
-for line in data:
-    words.extend(line.split(' '))
-print('The amount of words is', len(words))
-
-
-# counting the amount of characters
-characters = 0
-for line in data:
-    characters += len(line)
-print('The amount of characters is', characters)
+wc(f)
+f.close()
